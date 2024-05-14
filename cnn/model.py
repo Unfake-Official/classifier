fix: normalization + model loading

cnn/model.py
@@ -9,12 +9,19 @@ class Classifier(tf.keras.Model):
 
         # convolution + feature extraction
         # todo: change filters size
-        self.conv1 = tf.keras.layers.Conv2D(16, (16, 16), activation='relu')
+        self.conv1 = tf.keras.layers.Conv2D(16, (3, 3))
+        self.batch_norm1 = tf.keras.layers.BatchNormalization()
+        self.relu1 = tf.keras.layers.Activation('relu')
         self.max_pool1 = tf.keras.layers.MaxPool2D(pool_size=2, strides=2)
-        self.conv2 = tf.keras.layers.Conv2D(32, (16, 16), activation='relu')
+        self.conv2 = tf.keras.layers.Conv2D(32, (3, 3))
+        self.batch_norm2 = tf.keras.layers.BatchNormalization()
+        self.relu2 = tf.keras.layers.Activation('relu')
         self.max_pool2 = tf.keras.layers.MaxPool2D(pool_size=2, strides=2)
-        self.conv3 = tf.keras.layers.Conv2D(64, (16, 16), activation='relu')
+        self.conv3 = tf.keras.layers.Conv2D(64, (3, 3))
+        self.batch_norm3 = tf.keras.layers.BatchNormalization()
+        self.relu3 = tf.keras.layers.Activation('relu')
         self.max_pool3 = tf.keras.layers.MaxPool2D(pool_size=2, strides=2)
+        
         self.dropout = tf.keras.layers.Dropout(0.5)
 
         # dense layer + output
@@ -24,10 +31,16 @@ class Classifier(tf.keras.Model):
 
     def call(self, x):
         x = self.conv1(x)
+        x = self.batch_norm1(x)
+        x = self.relu1(x)
         x = self.max_pool1(x)
         x = self.conv2(x)
+        x = self.batch_norm2(x)
+        x = self.relu2(x)
         x = self.max_pool2(x)
         x = self.conv3(x)
+        x = self.batch_norm3(x)
+        x = self.relu3(x)
         x = self.max_pool3(x)
         x = self.dropout(x)
 

cnn/train.py
@@ -30,8 +30,8 @@ main_directory/
 DATASET_PATH = r'C:\Users\mcsgo\OneDrive\Documentos\Dataset'
 
 model = Classifier()
-# if os.path.exists(CHECKPOINT_PATH):
-#     model.load_weights(CHECKPOINT_PATH)
+if os.path.exists(CHECKPOINT_PATH):
+    model.load_model(CHECKPOINT_PATH)
 
 # todo: Configure dataset for performance (cache and prefetch)
 train_ds, test_ds = tf.keras.utils.image_dataset_from_directory(