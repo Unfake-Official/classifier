8f0e2fab716f67471fe7d7be0d6430b7a81db586
Marcos Godinho Filho <113925560+Marcos-Godinho-Filho@users.noreply.github.com>
2024-09-29 23:59:41 -0300

    fix: inference from audio using grayscale model

--- cnn/inference.py
+++ cnn/inference.py
@@ -1,21 +1,40 @@
 from classifier import Classifier
 import numpy as np
 from keras import utils, ops, Sequential, layers
+import tensorflow as tf
+import librosa
+from io import BytesIO
+import matplotlib.pyplot as plt
+import PIL.Image as img
 
 CHECKPOINT_PATH = 'cnn/checkpoints/model'
+
+IMG_SIZE = (256, 256)
 
 model = Classifier()
 model = Sequential([layers.TFSMLayer(CHECKPOINT_PATH, call_endpoint='serving_default')])
 
-IMG_PATH = r'C:\Users\mcsgo\Downloads\real.png'
-IMG_SIZE = (512, 256)
+AUDIO_PATH = r'C:\Users\mcsgo\OneDrive\Documentos\TCC\ASVSPOOF\LA\LA\ASVspoof2019_LA_eval\flac\LA_E_1000147.flac'
 
-class_names=['fake', 'real']
+class_names = ['fake', 'real']
 
-img = utils.load_img(IMG_PATH, grayscale=True, target_size=IMG_SIZE)
-img_array = utils.img_to_array(img)
+y, sr = librosa.load(AUDIO_PATH, sr=None)
+
+spec = np.abs(librosa.cqt(y, sr=sr))
+spec = librosa.amplitude_to_db(spec, ref=np.max)
+
+img_bytesio = BytesIO()
+plt.imsave(img_bytesio, spec, cmap='gray', format='png')
+img_bytesio.seek(0)
+
+image = img.open(img_bytesio)
+image = image.resize(IMG_SIZE)
+
+img_array = utils.img_to_array(image)
+img_array = tf.expand_dims(img_array, 0)
+img_array = tf.reshape(img_array, [-1, 256, 256, 1])
 
 predictions = model.predict(img_array)
-score = ops.nn.softmax(predictions[0])
+score = ops.nn.softmax(predictions['output_0'][0])
 
-print(f"Your audio is probably '{class_names[np.argmax(score)]}' with {100*np.max(score)}% confidence.")
+print(f"Your audio is probably '{class_names[np.argmax(score)]}' with {100*np.max(score):.2f}% confidence.")
